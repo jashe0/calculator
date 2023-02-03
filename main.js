777fffc2845cee8d2fe29761099903522476fbf5
Jonathan Ashe <120477825+jashe0@users.noreply.github.com>
2023-02-03 15:35:35 +0000

    create index main and styles pages plus simple console calculator

---
+++ main.js
@@ -0,0 +1,30 @@
+const add = function(numA,numB) {
+	let numC = numA + numB;
+  /**return numC;*/
+  console.log(numC);
+};
+
+const subtract = function(numA,numB) {
+  let numC = numA - numB;
+  /**return numC;	**/
+  console.log(numC);
+};
+
+const multiply = function(numA, numB) {
+ let numC = numA - numB;
+ console.log(numC);
+};
+
+const divide = function(numA,numB){
+    let numC = numA / numB;
+    /**return numC;**/
+    console.log(numC);
+}
+
+add(10,20);
+
+subtract(50,70);
+
+multiply(5,10);
+
+divide(100,50);
